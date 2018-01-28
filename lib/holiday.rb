3de24a37cc0e3104dff2dfba8aa6cb347a516484
Shirley Zhang <shirleyz9402@yahoo.com>
2018-01-28 01:18:21 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -60,7 +60,8 @@ def all_supplies_in_holidays(holiday_hash)
 holiday_hash.each do |season, holidays|
   puts "#{season.to_s.capitalize}:"
   holidays.each do |holiday,supplies|
-   puts "#{holiday.to_s.capitalize}:"
+   puts "#{holiday.to_s.capitalize}: #{supplies.split}"
+   
  end 
 end 
 end
