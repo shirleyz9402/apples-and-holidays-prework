b4919c3544da396f523bfa35b949050e976659ba
Shirley Zhang <shirleyz9402@yahoo.com>
2018-01-28 01:01:13 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -57,7 +57,7 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-
+holiday_hash.each do |season,|
 end
 
 def all_holidays_with_bbq(holiday_hash)
